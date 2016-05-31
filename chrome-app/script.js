65de92a565572001f01ba5de398a615394906f9c
Akira Hanashiro <hanashiro@users.noreply.github.com>
2016-05-31 11:24:24 -0300

    shortcuts

--- chrome-app/script.js
+++ chrome-app/script.js
@@ -1,3 +1,27 @@
+var PmxWebview = {
+	obj: document.getElementById('playmix'),
+	playPause: function(){
+		PmxWebview.obj.executeScript({'code': "document.getElementsByClassName('player-btn-play-pause')[0].click()"})
+	},
+	next: function(){
+		PmxWebview.obj.executeScript({'code': "document.getElementsByClassName('player-btn-next')[0].click()"})
+	},
+	previous: function(){
+		PmxWebview.obj.executeScript({'code': "document.getElementsByClassName('player-btn-previous')[0].click()"})
+	}
+}
+
+
+
 document.getElementById("pmx-btn-close").onclick = function() {
 	window.close();
 }
+
+
+chrome.commands.onCommand.addListener(function(command) { console.log(command, 151654)
+    switch(command){
+    	case 'play-pause': PmxWebview.playPause(); break;
+    	case 'next-track': PmxWebview.next(); break;
+    	case 'prev-track': PmxWebview.previous(); break;
+    }
+});
